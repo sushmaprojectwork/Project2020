7762b6aed8198c2de30654a170bb028f3726b643
sushmaprojectwork <65497197+sushmaprojectwork@users.noreply.github.com>
2020-05-17 19:38:52 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const path = require('path');
+
+let libraryName = 'js2flowchart';
+
+let outputFile;
+
+outputFile = libraryName + '.js';
+
+const config = {
+  entry: __dirname + '/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "(typeof window !== 'undefined' ? window : this)"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+        path.resolve('./node_modules'), path.resolve('./src'),
+        path.resolve('./src/shared'), path.resolve('./src/render'),
+        path.resolve('./src/builder'), path.resolve('./src/presentation-generator')
+    ],
+    extensions: ['.json', '.js']
+  },
+  mode: 'development'
+};
+
+module.exports = config;
